ba98e7319d2ccbc41cdc6335b5a55bdac6afed37
renzon <renzon@gmail.com>
2017-06-23 02:30:22 -0300

    Implemented docker for flags example

--- 17-futures/countries/vaurien_delay.sh
+++ 17-futures/countries/vaurien_delay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 vaurien --protocol http --backend localhost:8001 \
-        --proxy localhost:8002  \
-        --behavior 100:delay --behavior-delay-sleep .5
+        --proxy 0.0.0.0:8002  \
+        --behavior 100:delay --behavior-delay-sleep 1
--- 17-futures/countries/vaurien_error_delay.sh
+++ 17-futures/countries/vaurien_error_delay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 vaurien --protocol http --backend localhost:8001 \
-        --proxy localhost:8003  \
+        --proxy 0.0.0.0:8003  \
         --behavior 25:error,50:delay --behavior-delay-sleep .5
